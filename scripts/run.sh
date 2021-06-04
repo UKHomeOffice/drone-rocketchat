ab8527cb9d4c26a4fa6302a7e44981db593cade4
Qamar Admin <qamaradmin@DAS00802.local>
2021-06-04 16:45:06 +0100

    indentation fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
-
+PLUGIN_ICON=":sos:"
+PLUGIN_USERNAME="Test"
+PLUGIN_TITLE="Test"
+PLUGIN_TEXT="Test"
+PLUGIN_COLOR="#2DE0A5"
+PLUGIN_WEBHOOK="https://rocketchat.shs-dev.dsa-notprod.homeoffice.gov.uk/hooks/ePytNRCpvGinTaLPP/RW4nT4sLEvt4wxEhbYar4brxdKGdsFf8DoH4gd5AvGSKgjAz"
 # ROCKET_WEBHOOK=https://rocketchat.shs-dev.dsa-notprod.homeoffice.gov.uk/hooks/KsebptzxkYTvjg7Ji/QkwbaX3h3gNJ8gjiKRJx9RmgErvRtYA6pQ6XEEy3jy5DKpij
 generate_data()
 {
   cat <<EOF
   {
-   "icon_emoji": "${PLUGIN_ICON}",
+   "icon_emoji": ${PLUGIN_ICON},
    "username": ${PLUGIN_USERNAME},
    "attachments": [ {
     "title": ${PLUGIN_TITLE},
-    "text": ${PLUGIN_MESSAGE},
-    "color":${PLUGIN_COLOR}}]
+    "text": ${PLUGIN_TEXT},
+    "color": ${PLUGIN_COLOR}
+    } ]
   }
 EOF
 }

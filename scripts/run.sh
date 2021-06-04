563087b725f18e081237e95cc169ef0239693a9c
Qamar Admin <qamaradmin@DAS00802.local>
2021-06-04 17:04:52 +0100

    changing script to inlcude echos

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-PLUGIN_ICON=":sos:"
-PLUGIN_USERNAME="Test"
-PLUGIN_TITLE="Test"
-PLUGIN_TEXT="Test"
-PLUGIN_COLOR="#2DE0A5"
-PLUGIN_WEBHOOK="https://rocketchat.shs-dev.dsa-notprod.homeoffice.gov.uk/hooks/ePytNRCpvGinTaLPP/RW4nT4sLEvt4wxEhbYar4brxdKGdsFf8DoH4gd5AvGSKgjAz"
 # ROCKET_WEBHOOK=https://rocketchat.shs-dev.dsa-notprod.homeoffice.gov.uk/hooks/KsebptzxkYTvjg7Ji/QkwbaX3h3gNJ8gjiKRJx9RmgErvRtYA6pQ6XEEy3jy5DKpij
 generate_data()
 {
@@ -20,7 +14,13 @@ generate_data()
   }
 EOF
 }
-
+echo "Invoking webhook"
 curl -k -X POST \
 -H 'Content-type: application/json' \
 --data "${generate_data}" ${PLUGIN_WEBHOOK}
+
+if [ $? -eq 0 ]; then
+    echo "Curl ok"
+else
+    echo "Curl failed"
+fi

fcb2a07c099ee128f517339a71e9363bfcd51823
Qamar Admin <qamaradmin@DAS00802.local>
2021-06-07 12:28:13 +0100

    Put data payload into variable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
-# ROCKET_WEBHOOK=https://rocketchat.shs-dev.dsa-notprod.homeoffice.gov.uk/hooks/KsebptzxkYTvjg7Ji/QkwbaX3h3gNJ8gjiKRJx9RmgErvRtYA6pQ6XEEy3jy5DKpij
-generate_data()
-{
-  cat <<EOF
-  {
-   "icon_emoji": ${PLUGIN_ICON},
-   "username": ${PLUGIN_USERNAME},
-   "attachments": [ {
+
+generate_data() {
+  cat <<EOF 
+ {
+    "icon": ${PLUGIN_ICON},
+    "username": ${PLUGIN_USERNAME},
+    "attachments": [ {
     "title": ${PLUGIN_TITLE},
     "text": ${PLUGIN_TEXT},
     "color": ${PLUGIN_COLOR}
     } ]
-  }
+ }
 EOF
 }
+output=$(generate_data)
+
 echo "Invoking webhook"
 curl -k -X POST \
 -H 'Content-type: application/json' \
---data "${generate_data}" ${PLUGIN_WEBHOOK}
-
-if [ $? -eq 0 ]; then
-    echo "Curl ok"
-else
-    echo "Curl failed"
-fi
+--data "${output}" ${PLUGIN_WEBHOOK}

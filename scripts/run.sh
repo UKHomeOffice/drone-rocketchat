e8267ab272ac1c1619f7ab4b2d288b2bf70b5000
Qamar Admin <qamaradmin@DAS00802.local>
2021-06-07 14:04:13 +0100

    fixing a bug

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
-shopt -s extglob
 
 generate_data() {
   cat <<EOF 
  {
-    "icon_emoji": ${PLUGIN_ICON},
-    "username": ${PLUGIN_USERNAME},
+    "icon_emoji": "${PLUGIN_ICON}",
+    "username": "${PLUGIN_USERNAME}",
     "attachments": [ {
-    "title": ${PLUGIN_TITLE},
-    "text": ${PLUGIN_TEXT},
-    "color": ${PLUGIN_COLOR}
+    "title": "${PLUGIN_TITLE}",
+    "text": "${PLUGIN_TEXT}",
+    "color": "${PLUGIN_COLOR}"
     } ]
  }
 EOF
 }
-data="$(generate_data)"
+data=$(generate_data)
+echo $data
+
 echo "Invoking webhook"
+
 curl -k -X POST \
 -H 'Content-type: application/json' \
---data $(data) ${PLUGIN_WEBHOOK}
+--data "${data}" ${PLUGIN_WEBHOOK}
